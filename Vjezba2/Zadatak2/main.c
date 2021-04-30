b73acfe8eeb36fa4553dcf5f43929c411ed4d61f
“jana-jankovic” <janajankoviic@gmail.com>
2021-04-30 22:30:48 +0200

    PWM

--- Vjezba2/Zadatak2/main.c
+++ Vjezba2/Zadatak2/main.c
@@ -2,15 +2,25 @@
 #include <avr/interrupt.h>
 
 unsigned long millis=0;
+unsigned char fi=50;
 
 ISR(TIMER0_COMPA_vect)
 {
 	millis++;
+
+	if(millis == 256) millis=0;
+
+	if(millis < 250)
+		PORTB |= 1 << 5;
+	else
+	{
+		PORTB &= ~(1 << 5);
+	}
 }
 
 int main()
 {
-	DDRB |= 1 << 4;
+	DDRB |= 1 << 5;
 
 	TCCR0A = 0x02;
 	TCCR0B = 0x01;
