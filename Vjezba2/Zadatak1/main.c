3d10811eec29c92be3c90076ae49586d9047b63e
“jana-jankovic” <janajankoviic@gmail.com>
2021-04-30 17:10:42 +0200

    zad1 pod b

--- Vjezba2/Zadatak1/main.c
+++ Vjezba2/Zadatak1/main.c
@@ -1,16 +1,37 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+
+unsigned char t0_cnt = 0;
+
+
+ISR(TIMER0_COMPA_vect)
+{
+	t0_cnt++;
+
+	if(t0_cnt == 125)
+	{
+		t0_cnt = 0;
+		PORTB ^= 1 << 5;
+	}
+
+}
+
 
 int main()
 {
-	DDRB |= (1 << 5);
-	while(1)
-	{
-		PORTB |= (1 << 5);
-		_delay_ms(1000);
-		PORTB &= ~(1 << 5);
-	    _delay_ms(1000);
-	}
+	DDRB |= 1 << 5; //PB5 je izlaz
+	PORTB |= 1 << 5;
+
+	TCCR0A = 0x02; //tajmer 0: CTC mod
+	TCCR0B = 0x04; //tajmer 0: fclk = fosc/256
+	 OCR0A = 124;
+
+	TIMSK0 = 0x02; //dozvola prekida tajmera 0
+
+	sei(); //I = 1 (dozvola prekida)
+
+	while (1);
 
 	return 0;
 }
+
